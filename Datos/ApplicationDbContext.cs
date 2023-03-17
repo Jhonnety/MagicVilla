f4fcdb136e9a0d25a6c86c87683a25da6e911cb4
Jhonnety <jhonestebanvelasquezgomez@gmail.com>
2023-03-17 11:24:17 -0500

    DbContext - inyeccion de dependencias

---
+++ Datos/ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using MagicVilla_Api.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_Api.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        //usar add-migration nombreCuaquiera para crear la migracion del Modelo a la tabla de la base de datos, tambien crear la base de datos
+        //update-database ejecutar script anterior
+        {
+
+        }
+
+        //definimos los modelos que queremos que se creen como tablas en la base de datos
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                    new Villa()
+                    {
+                        Id = 1,
+                        Nombre = "Villa real",
+                        Detalle = "Detalle de la villa",
+                        ImageUrl = "",
+                        Ocupantes = 5,
+                        MetrosCuadrados = 50,
+                        Tarifa = 200100,
+                        Amenida = "",
+                        FechaCreacion = DateTime.Now,
+                        FechaActualizacion = DateTime.Now
+                    },
+                    new Villa()
+                    {
+                        Id = 2,
+                        Nombre = "Villa premiun vista a la playa",
+                        Detalle = "Detalle de la villa",
+                        ImageUrl = "",
+                        Ocupantes = 4,
+                        MetrosCuadrados = 60,
+                        Tarifa = 500000,
+                        Amenida = "",
+                        FechaCreacion = DateTime.Now,
+                        FechaActualizacion = DateTime.Now
+                    }
+                );
+        }
+
+    }
+}
---
+++ Modelos/Dto/VillaDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MagicVilla_Api.Modelos.Dto
+{
+    public class VillaDto
+    {
+        //Los Dto son para exponer solo los datos que quiero del modelo
+        //Entity framework core ese usa para trabajar con base de datos,
+        //es un orm que maneja las operaciones de la base de datos, en lugar de usar queries
+        //Microsoft.EntityFrameworkCore.SqlServe
+        //Microsoft.EntityFrameworkCore.Tools
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+        public string Detalle { get; set; }
+        [Required]
+        public double Tarifa { get; set; }
+        public int Ocupantes { get; set; }
+        public int MetrosCuadrados { get; set; }
+        public string ImageUrl { get; set; }
+        public string Amenida { get; set; }
+    }
+}
